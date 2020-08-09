27af5f4b542b4fa666f1154f5cc339af561c1fb2
Saalik Lokhandwala <saaliklok@pop-os.localdomain>
2020-08-08 20:06:46 -0400

    prepped playground for prod

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const resolvers = {
 }
 
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, introspection:true, playground:true })
 server.listen(
     port = process.env.PORT || 4000
 ).then(({ url }) => {
